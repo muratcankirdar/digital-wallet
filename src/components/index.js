7bcc1a7d71481802ae4ac4b243bb13f78c282824
muratcankirdar <muratcankirdar@gmail.com>
2022-06-18 15:34:08 +0300

    export bottom navigation component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import AddCardButton from "./AddCardButton";
 import NameSection from "./NameSection";
 import Transactions from "./Transactions";
 import ExpenseItem from "./ExpenseItem";
+import BottomNavigation from './BottomNavigation';
 
-export {CreditCard, AddCardButton, NameSection, Transactions, ExpenseItem};
+export {CreditCard, AddCardButton, NameSection, Transactions, ExpenseItem, BottomNavigation};
