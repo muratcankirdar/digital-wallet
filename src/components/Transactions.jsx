fa1e65ec544ffa6d69bf8e377cc17d060834cc90
muratcankirdar <muratcankirdar@gmail.com>
2022-06-18 14:40:07 +0300

    add dropdown menu to Transactions

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,6 +1,12 @@
+import { useState } from 'react';
 import { ExpenseItem } from './index';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
 
 const Transactions = () => {
+  const [currency, setCurrency] = useState('USD');
+
   return (
     <div className="transactions">
       <div className="transactions__top">
@@ -17,7 +23,17 @@ const Transactions = () => {
         </div>
 
         <div className="currency-selection">
-          {/* dropdown */}
+          <FormControl sx={{ m: 0, width: 110 }} size="small">
+            <Select
+              value={currency}
+              onChange={(event) => setCurrency(event.target.value)}
+            >
+              <MenuItem value="USD">USD</MenuItem>
+              <MenuItem value="TRY">TL</MenuItem>
+              <MenuItem value="EUR">EUR</MenuItem>
+              <MenuItem value="CHF">CHF</MenuItem>
+            </Select>
+          </FormControl>
         </div>
       </div>
 
