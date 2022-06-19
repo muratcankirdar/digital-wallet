a0f31b05959fbcc88b7e902f94aa2d8ab6fd6f16
muratcankirdar <muratcankirdar@gmail.com>
2022-06-19 10:56:30 +0300

    implement dynamic data from request

--- src/components/NameSection.jsx
+++ src/components/NameSection.jsx
@@ -1,27 +1,31 @@
-const NameSection = ({ name, gender }) => {
-  const genderText = gender === 'male' ? 'Mr.' :  gender === 'female' ? 'Mrs.' : '';
-  const GreetingMessage = () => {
-    const date = new Date();
-    const currentHour = date.getHours();
-    let message = 'Good Morning';
+const GreetingMessage = () => {
+  const date = new Date();
+  const currentHour = date.getHours();
+  let message = 'Good Morning';
 
-    if (currentHour >= 12 && currentHour <= 18) {
-      message = 'Good Afternoon';
-    } else if (currentHour >= 18 && currentHour < 6) {
-      message = 'Good Evening';
-    }
-
-    return <span className="title">{message}</span>;
+  if (currentHour >= 12 && currentHour <= 18) {
+    message = 'Good Afternoon';
+  } else if (currentHour >= 18 && currentHour < 6) {
+    message = 'Good Evening';
   }
+
+  return <span className="title">{message}</span>;
+};
+
+const NameSection = (
+  {
+    name = '',
+    surname = '',
+    gender = '',
+  }) => {
+  const genderText = gender === 'male' ? 'Mr.' : gender === 'female' ? 'Mrs.' : '';
 
   return (
     <div className="name-section">
-      <GreetingMessage />
-
+      <GreetingMessage/>
       <br/>
-
       <span className="name">
-        {genderText} {name}
+        {genderText} {name} {surname}
       </span>
     </div>
   );
