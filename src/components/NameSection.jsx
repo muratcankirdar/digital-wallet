6267794be21332b8858c31788fa02b9a6dd09502
muratcankirdar <muratcankirdar@gmail.com>
2022-06-19 09:21:23 +0300

    handle good morning, afternoon and evening texts on NameSection

--- src/components/NameSection.jsx
+++ src/components/NameSection.jsx
@@ -1,11 +1,22 @@
 const NameSection = ({ name, gender }) => {
   const genderText = gender === 'male' ? 'Mr.' :  gender === 'female' ? 'Mrs.' : '';
+  const GreetingMessage = () => {
+    const date = new Date();
+    const currentHour = date.getHours();
+    let message = 'Good Morning';
+
+    if (currentHour >= 12 && currentHour <= 18) {
+      message = 'Good Afternoon';
+    } else if (currentHour >= 18 && currentHour < 6) {
+      message = 'Good Evening';
+    }
+
+    return <span className="title">{message}</span>;
+  }
 
   return (
     <div className="name-section">
-      <span className="title">
-        Good Morning
-      </span>
+      <GreetingMessage />
 
       <br/>
 
