9b758337daad2ceb9492b94e2ea87098d81493cf
muratcankirdar <muratcankirdar@gmail.com>
2022-06-19 09:09:10 +0300

    fix gender prop to check design

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <div className="name-container">
-        <NameSection name="Zayn Malik" gender="malde"/>
+        <NameSection name="Zayn Malik" gender="male"/>
       </div>
 
       <div className="wrapper">
